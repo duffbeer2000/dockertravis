436c026ed34f844e8199078927444ce2fb591add
duffbeer2000 <duffbeer2000@users.noreply.github.com>
2019-04-07 13:51:30 +0200

    test

--- armv5e-min/scripts/iobroker_startup.sh
+++ armv5e-min/scripts/iobroker_startup.sh
@@ -5,35 +5,64 @@ avahi=$AVAHI
 ADMIN_PORT=$IOBROKER_ADMIN_PORT
 WEB_PORT=$IOBROKER_WEB_PORT
 
-#Deklarate variables
+#Declarate variables
+version="0.2.0 beta"
 HOSTNAME_NEW=$(hostname)
+IOB_USER="iobroker"
+IOB_DIR="/opt/iobroker"
 
 # Getting date and time for logging 
 dati=`date '+%Y-%m-%d %H:%M:%S'`
 
-# Information
-echo ''
-echo '----------------------------------------'
-echo '-----   Image-Version: 0.2.0 beta  -----'
-echo '-----      '$dati'      -----'
-echo '----------------------------------------'
-echo ''
-echo 'Startupscript running...'
+# Enable colored output
+if test -t 1; then # if terminal
+	ncolors=$(which tput > /dev/null && tput colors) # supports color
+	if test -n "$ncolors" && test $ncolors -ge 8; then
+		termcols=$(tput cols)
+		bold="$(tput bold)"
+		underline="$(tput smul)"
+		standout="$(tput smso)"
+		normal="$(tput sgr0)"
+		black="$(tput setaf 0)"
+		red="$(tput setaf 1)"
+		green="$(tput setaf 2)"
+		yellow="$(tput setaf 3)"
+		blue="$(tput setaf 4)"
+		magenta="$(tput setaf 5)"
+		cyan="$(tput setaf 6)"
+		white="$(tput setaf 7)"
+	fi
+fi
 
-# Restoring if ioBroker-folder empty
-cd /opt/iobroker
-if [ `ls -1a|wc -l` -lt 3 ];
-then
+#Check if actual user is root
+if [[ $EUID -eq 0 ]]; then
+	#echo 'Is Root'
+	IS_ROOT=true
+else
+	#echo 'Is not root'
+	IS_ROOT=false
+fi
+
+# Functions
+avahi_init() {
+	# Setting up avahi-daemon
+	echo ''
+	echo 'Initializing Avahi-Daemon...'
+	sudo bash /opt/scripts/avahi_startup.sh $1
+	echo 'Initializing Avahi-Daemon done...'
+}
+
+restore_iobroker_folder() {
+	# Restoring ioBroker-folder
 	echo ''
 	echo 'Directory /opt/iobroker is empty!'
 	echo 'Restoring...'
-	cd /opt/iobroker
 	tar -xf /opt/initial_iobroker.tar -C /
 	echo 'Restoring done...'
-fi
+	
+}
 
-if [ -f /opt/scripts/.install_host ];
-then
+first_run_prep() {
 	HOSTNAME_OLD=$(cat /opt/scripts/.install_host)
 	echo ''
 	echo 'First run preparation! Used Hostname:' $(hostname)
@@ -42,18 +71,36 @@ then
 	rm -f /opt/scripts/.install_host
 	echo 'ioBroker renamed...'
 	echo 'First run preparation done...'
+}
+
+# Information
+echo ''
+echo '----------------------------------------'
+echo '---- Image-Version: '$version'-Min  ----'
+echo '-----      '$dati'      -----'
+echo '----------------------------------------'
+echo ''
+echo 'Startupscript running...'
+# Show installed Node- and NPM-Version
+echo "Node-Version:   " $(node -v)
+echo "NodeJs-Version: " $(nodejs -v)
+echo "Npm-Version:    " $(npm -v)
+
+# Restoring if ioBroker-folder empty
+cd /opt/iobroker
+if [ `ls -1a|wc -l` -lt 3 ]; then
+	restore_iobroker_folder
+fi
+
+# Check if installation is updated or new
+if [ -f /opt/scripts/.install_host ]; then
+	first_run_prep
 fi
 
 # Checking for and setting up avahi-daemon
-if [ "$avahi" = "true" ]
-then
-	echo ''
-	echo 'Initializing Avahi-Daemon...'
-	sudo bash /opt/scripts/avahi_startup.sh
-	echo 'Initializing Avahi-Daemon done...'
+if [ "$avahi" = "1" ]; then
+	avahi_init
 fi
-
-sleep 5
 
 if [[ ${ADMIN_PORT} != *"8081"* ]]; then
 	cd /opt/iobroker
@@ -64,7 +111,6 @@ if [[ ${WEB_PORT} != *"8082"* ]]; then
 	cd /opt/iobroker
 	iobroker set web.0 --port ${WEB_PORT}
 fi
-
 
 # Starting ioBroker
 echo ''
--- armv6-min/scripts/avahi_startup.sh
+++ armv6-min/scripts/avahi_startup.sh
@@ -4,16 +4,16 @@ echo 'Checking avahi-daemon installation state...'
 
 if [ -f /usr/sbin/avahi-daemon ]
 then
-  echo 'Avahi already installed...'
+	echo 'Avahi already installed...'
 else
-  echo 'Installing avahi-daemon...'
-  apt-get update > /opt/scripts/avahi_startup.log 2>&1
-  apt-get install -y libavahi-compat-libdnssd-dev avahi-daemon >> /opt/scripts/avahi_startup.log 2>&1
-  rm -rf /var/lib/apt/lists/* >> /opt/scripts/avahi_startup.log 2>&1
-  echo 'Configuring avahi-daemon...'
-  sed -i '/^rlimit-nproc/s/^\(.*\)/#\1/g' /etc/avahi/avahi-daemon.conf
-  echo 'Configuring dbus...'
-  mkdir /var/run/dbus/
+	echo 'Installing avahi-daemon...'
+	apt-get update > /opt/scripts/avahi_startup.log 2>&1
+	apt-get install -y libavahi-compat-libdnssd-dev avahi-daemon >> /opt/scripts/avahi_startup.log 2>&1
+	rm -rf /var/lib/apt/lists/* >> /opt/scripts/avahi_startup.log 2>&1
+	echo 'Configuring avahi-daemon...'
+	sed -i '/^rlimit-nproc/s/^\(.*\)/#\1/g' /etc/avahi/avahi-daemon.conf
+	echo 'Configuring dbus...'
+	mkdir /var/run/dbus/
 fi
 
 
