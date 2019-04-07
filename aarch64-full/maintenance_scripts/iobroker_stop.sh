21c04e42e10906001e65fa3bec4222dfac7aa45c
duffbeer2000 <duffbeer2000@users.noreply.github.com>
2019-04-07 17:32:19 +0200

    test easyer travis push

--- aarch64-full/maintenance_scripts/iobroker_stop.sh
+++ aarch64-full/maintenance_scripts/iobroker_stop.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 # Einfaches Script zum Stoppen von ioBroker. 
-# Kann zum Beispiel aus ioBroker heraus aufgerufen werden um ioBroker zu stoppen. 
 
 cd /opt/iobroker
 pkill io
